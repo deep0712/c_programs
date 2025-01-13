97301a3f246d8ab339907ed8cb1fa94285e877e9
deep0712 <pradeepkumawat99618@gmail.com>
2025-01-13 11:30:03 +0530

    strassen's algo

---
+++ strassen.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<vector<int>> Matrix;
+
+Matrix add(const Matrix &A, const Matrix &B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] + B[i][j];
+    return C;
+}
+
+Matrix subtract(const Matrix &A, const Matrix &B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] - B[i][j];
+    return C;
+}
+
+Matrix strassen(const Matrix &A, const Matrix &B) {
+    int n = A.size();
+    if (n == 1) {
+        return Matrix{{A[0][0] * B[0][0]}};
+    }
+  
+    int k = n / 2;
+    Matrix A11(k, vector<int>(k)), A12(k, vector<int>(k)), A21(k, vector<int>(k)), A22(k, vector<int>(k));
+    Matrix B11(k, vector<int>(k)), B12(k, vector<int>(k)), B21(k, vector<int>(k)), B22(k, vector<int>(k));
+
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            A11[i][j] = A[i][j];
+            A12[i][j] = A[i][j + k];
+            A21[i][j] = A[i + k][j];
+            A22[i][j] = A[i + k][j + k];
+
+            B11[i][j] = B[i][j];
+            B12[i][j] = B[i][j + k];
+            B21[i][j] = B[i + k][j];
+            B22[i][j] = B[i + k][j + k];
+        }
+    }
+
+    Matrix M1 = strassen(add(A11, A22), add(B11, B22));
+    Matrix M2 = strassen(add(A21, A22), B11);
+    Matrix M3 = strassen(A11, subtract(B12, B22));
+    Matrix M4 = strassen(A22, subtract(B21, B11));
+    Matrix M5 = strassen(add(A11, A12), B22);
+    Matrix M6 = strassen(subtract(A21, A11), add(B11, B12));
+    Matrix M7 = strassen(subtract(A12, A22), add(B21, B22));
+
+    Matrix C(n, vector<int>(n));
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            C[i][j] = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
+            C[i][j + k] = M3[i][j] + M5[i][j];
+            C[i + k][j] = M2[i][j] + M4[i][j];
+            C[i + k][j + k] = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
+        }
+    }
+    
+    return C;
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the matrices (power of 2): ";
+    cin >> n;
+
+    Matrix A(n, vector<int>(n)), B(n, vector<int>(n));
+    
+    cout << "Enter matrix A:" << endl;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> A[i][j];
+
+    cout << "Enter matrix B:" << endl;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> B[i][j];
+
+    Matrix C = strassen(A, B);
+
+    cout << "Resultant matrix after multiplication:" << endl;
+    for (const auto &row : C) {
+        for (const auto &val : row)
+            cout << val << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
